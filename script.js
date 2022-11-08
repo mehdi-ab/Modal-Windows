525094c86e1a1eb49e0c852f2529c99b3696bd54
Mehdi <medpurple1969@hotmail.com>
2022-11-08 15:13:54 +0100

    Ajout Echap to remove class

--- script.js
+++ script.js
@@ -22,3 +22,11 @@ for (let i = 0; i < btnsOpenModal.length; i++)
 
     btnCloseModal.addEventListener('click', closeModal);
     overlay.addEventListener('click', closeModal);
+
+document.body.addEventListener('keydown', function(e) {
+    if(e.key === 'Escape') {
+        if(!modal.classList.contains('hidden')) {
+            closeModal();
+        }
+    }
+})
